219abd1a658cdca69b555600e88565b750755146
wordaddict_ <madeyinka@terragonltd.com>
2018-08-31 16:25:28 +0100

    added models for the trivia

---
+++ models/trivia.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const moment = require('moment');
+const { Schema } = mongoose;
+
+const triviaSchema = new Schema({
+  question: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  answer: {
+    type: [String],
+    required: false,
+    default: []
+  },
+  hint: {
+    type: String,
+    default: ''
+  },
+  // changed to an Array from an Object
+  options: {
+    type: [String],
+    required: false,
+    default: []
+  },
+  difficulty: {
+    type: String,
+    default: 'EASY',
+    enum: ['EASY', 'NORMAL', 'HARD']
+  },
+  createdAt: {
+    type: Date,
+    default: moment()
+  },
+  updatedAt: {
+    type: Date,
+    default: moment(),
+    select: false
+  }
+});
+
+
+const triviaModel = mongoose.model('trivial', triviaSchema);
+module.exports = triviaModel;
